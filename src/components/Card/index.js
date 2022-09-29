4ac9d0ce01f45e51527925bb3e9cba69fc5b9777
Sebastian Scherbes <github@scherbes.de>
2022-09-29 17:06:33 +0200

    create components

---
+++ src/components/Card/index.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export default function Card() {
+  return (
+    <CardItem>
+      <Picture
+        src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+        alt="Morty Smith"
+      />
+      <Name>Morty Smith</Name>
+      <ShowButton>Show more</ShowButton>
+    </CardItem>
+  );
+}
+
+const CardItem = styled.li`
+  list-style: none;
+  border-radius: 0.5rem;
+  background-color: pink;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: rgb(0 0 0) 0px 0px 8px 0px;
+`;
+
+const Picture = styled.img`
+  border-top-left-radius: 0.5rem;
+  border-top-right-radius: 0.5rem;
+`;
+
+const Name = styled.h2`
+  margin-top: 1rem;
+  text-align: center;
+`;
+
+const ShowButton = styled.button`
+  margin: 1rem 0;
+  width: 10rem;
+  height: 3rem;
+  font-size: 1.2em;
+  border: none;
+`;
