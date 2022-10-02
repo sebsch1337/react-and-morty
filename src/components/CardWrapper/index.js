f3631c5d4a1d5c6cb32dd147372f6e3c3de2ca92
Sebastian Scherbes <github@scherbes.de>
2022-10-02 17:44:14 +0200

    add question mark at pageload

--- src/components/CardWrapper/index.js
+++ src/components/CardWrapper/index.js
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid, regular } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 import Card from "../Card";
 
@@ -14,26 +18,25 @@ export default function CardWrapper({
   let { characterId } = useParams();
   let displayCharacters = characters;
 
-  if (randomPage) {
-    characterId = Math.random() * characters.length + 1;
-  }
+  const [randomId, setRandomId] = useState(0);
+
+  const getRandomNumber = () => parseInt(Math.random() * characters.length + 1);
 
   if (detailPage || randomPage) {
     characterId = parseInt(characterId);
     displayCharacters = characters.filter(
-      (character) => character.id === characterId
+      (character) => character.id === (randomPage ? randomId : characterId)
     );
   }
 
   if (favoritesPage) {
-    console.log(characters, bookmarks);
     displayCharacters = characters.filter((character) =>
       bookmarks.find((bookmark) => bookmark === character.id)
     );
   }
 
   return (
-    <CardList>
+    <CardList randomPage={randomPage}>
       {displayCharacters.map((character) => (
         <Card
           key={character.id}
@@ -45,13 +48,36 @@ export default function CardWrapper({
           detailPage={detailPage}
         />
       ))}
+      {randomPage && randomId === 0 && (
+        <QuestionMark>
+          <FontAwesomeIcon icon={solid("question")} size="10x" />
+        </QuestionMark>
+      )}
+      {randomPage && (
+        <RandomizeButton onClick={() => setRandomId(getRandomNumber())}>
+          Get random character
+        </RandomizeButton>
+      )}
     </CardList>
   );
 }
 
 const CardList = styled.ul`
   display: flex;
+  flex-direction: ${({ randomPage }) => (randomPage ? "column" : "row")};
   justify-content: center;
+  align-items: center;
   flex-wrap: wrap;
   gap: 1.5rem;
+`;
+
+const QuestionMark = styled.span`
+  display: flex;
+  justify-content: center;
+`;
+
+const RandomizeButton = styled.button`
+  border: 2px solid black;
+  padding: 1em;
+  font-size: 1.2rem;
 `;
